dc33c72201316b1666d425524c1f59f6d362e54d
Andrew <deyneka2003@gmail.com>
2024-03-28 00:27:22 +0200

    fix getAllHeroes axios

--- src/services/hero-service.ts
+++ src/services/hero-service.ts
@@ -7,12 +7,12 @@ axios.defaults.baseURL = API_URL;
 
 export const HeroService = {
   async getAllHeroes() {
-    const {data} = await axios.get<Hero[]>('/people');
+    const {data} = await axios.get('/people');
     return data.results
   },
 
   async getHeroesById(id: string) {
-    const {data} = await axios.get<Hero[]>(`/people`, {
+    const {data} = await axios.get(`/people`, {
       params: {
         id
       }

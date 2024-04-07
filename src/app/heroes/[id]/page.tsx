f56b5cb0f437e2362341e7a86d15f8b1b237e876
Andrew <deyneka2003@gmail.com>
2024-04-08 02:30:49 +0300

    add hero graph, using react flow

--- src/app/heroes/[id]/page.tsx
+++ src/app/heroes/[id]/page.tsx
@@ -3,6 +3,7 @@ import HeroDetail from '@/components/HeroDetail';
 import { Hero } from '@/interfaces/hero-interface';
 import FilmList from '@/components/FilmList';
 import ShipList from '@/components/ShipList';
+import HeroGraph from '@/components/HeroGraph';
 
 const HeroPage = async ({ params }: { params: { id: string } }) => {
   const hero: Hero = await HeroService.getHeroesById(params.id);
@@ -10,15 +11,14 @@ const HeroPage = async ({ params }: { params: { id: string } }) => {
   return (
     <div className="container mx-auto p-4">
       <div className="max-w-3xl mx-auto bg-gray-800 rounded-lg shadow-lg overflow-hidden">
-          <img className="w-full h-85 object-cover object-top" src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg"
-              alt="Star Wars baner" />
           <div className="p-6">
             <HeroDetail hero={hero} />
-          <div className="grid grid-cols-2 gap-4">
-            <FilmList films={hero.films} />   
-            <ShipList ships={hero.starships} />
+            <div className="grid grid-cols-2 gap-4">
+              <FilmList films={hero.films} />   
+              <ShipList ships={hero.starships} />
+            </div>
           </div>
-          </div>
+          <HeroGraph hero={hero} />
         </div>
       </div>
   );
